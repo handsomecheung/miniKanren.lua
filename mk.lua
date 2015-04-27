81c223d63bf516901484ae15e66a3fca5636496b
Handsome Cheung <handsomecheung@gmail.com>
2015-04-27 17:28:36 +0800

    do no use deepcopy in list_append()

--- mk.lua
+++ mk.lua
@@ -9,23 +9,11 @@ local function fresh_vars(n)
    return unpack(vars)
 end
 
-local function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' and (not is_var(orig)) then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else
-        copy = orig
-    end
-    return copy
-end
-
 local function list_append(l, e)
-   local new_list = deepcopy(l)
+   local new_list = {}
+   for _, v in ipairs(l) do
+      table.insert(new_list, v)
+   end
    table.insert(new_list, e)
    return new_list
 end
